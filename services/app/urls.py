b0582ebe51ec67abd78f5b729283df7b11bf62fa
Janek Horubała <jan.horubala@gmail.com>
2015-06-28 03:11:19 +0200

    beacons admin in services

--- services/app/urls.py
+++ services/app/urls.py
@@ -1,8 +1,9 @@
-from app.views import VolumeView, SlidesView, PrintView
+from app.views import VolumeView, SlidesView, PrintView, AdminView
 from django.conf.urls import url
 
 urlpatterns = [
     url(r'^slides/?$', SlidesView.as_view(), name="slides"),
     url(r'^volume/?$', VolumeView.as_view(), name="volume"),
     url(r'^print/?$', PrintView.as_view(), name="print"),
+    url(r'^admin/?$', AdminView.as_view(), name="admin"),
 ]
